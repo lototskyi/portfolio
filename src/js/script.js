5defa41bb84cc693bfea457b8600f4547a7e79a6
alex <o.lototskyi@gmail.com>
2022-09-13 12:59:14 +0300

    added auto skill raiting

--- src/js/script.js
+++ src/js/script.js
@@ -9,3 +9,20 @@ hamburger.addEventListener('click', () => {
 close.addEventListener('click', () => {
     menu.classList.remove('active');
 });
+
+//skills raiting
+const percents = document.querySelectorAll('.skills__tech-perc-item-perc'),
+      lines = document.querySelectorAll('.skills__tech-perc-item-progress');
+
+percents.forEach((item, i) => {
+    const regex = /-?\d+/;
+    const perc = +item.innerHTML.match(regex);
+    console.log(perc);
+    if (0 <= perc && perc <= 100) {
+        lines[i].style.width = `${perc}%`;
+    } else if(perc > 100) {
+        lines[i].style.width = `100%`;
+    } else {
+        lines[i].style.width = `0%`;
+    }
+});
